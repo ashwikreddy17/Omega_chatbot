5070a1fa6109702f6130f24d56bcac7cc2e6978d
ashwikreddy17 <ashwikreddy0917@gmail.com>
2025-06-08 11:21:42 +0530

    Add files via upload

---
+++ download_nltk_data.py
@@ -0,0 +1,6 @@
+import nltk
+
+# Download all required NLTK data into a local folder called 'nltk_data'
+nltk.download('punkt', download_dir='nltk_data')
+nltk.download('wordnet', download_dir='nltk_data')
+nltk.download('stopwords', download_dir='nltk_data')
